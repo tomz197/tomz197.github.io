997707a1e8b6184466d71d1f61ff7c8344ec867c
Tomáš <tomas.zifcak197@gmail.com>
2022-06-03 12:16:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './sections/LandingPage/LandingPage.js';
+import About from './sections/About/About.js';
+import Contact from './sections/Contact/Contact.js';
+import Header from './sections/Header/Header.js';
+import Projects from './sections/Projects/Projects.js';
+import Skills from './sections/Skills/Skills.js';
+import Footer from './sections/Footer/Footer.js';
+import Interests from './sections/Interests/Interests.js';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ overflowX: 'hidden' }}>
+      <Header className="header"></Header>
+      <LandingPage id="LandingPage"></LandingPage>
+      <About></About>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Interests></Interests>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
